30e0b6fb93e922a772510067c04cda2f6b8bc8b9
lianchao <18900945189@163.com>
2020-05-17 11:12:00 +0800

    引导叶首次提交

---
+++ app/src/main/java/activity/ljsw/com/leanprogrom/activity/ShalpActivity.java
@@ -0,0 +1,18 @@
+package activity.ljsw.com.leanprogrom.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import activity.ljsw.com.leanprogrom.R;
+
+/***
+ * 引导页代码
+ */
+public class ShalpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shalp);
+    }
+}
